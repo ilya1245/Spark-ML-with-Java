4a68934d86aab6914f60ea9cd8febbafcbec08a7
Ilya <ilya1245@gmail.com>
2019-01-28 13:02:06 +0200

    init structure

---
+++ Initial investigation/src/main/java/midway/spark/edu/Test.java
@@ -0,0 +1,13 @@
+package midway.spark.edu;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Car type -> Abarth");
+		System.out.println("Car type -> AlfaRomeo");
+		System.out.println("Car type -> Autobianchi");
+		System.out.println("Car type -> Bizzarinni");
+		System.out.println("Car type -> Bugatti");
+	}
+
+}
